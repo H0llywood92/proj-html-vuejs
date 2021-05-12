709d4c8dbcdb80de14bb16e6bb0bdc0bdbb7cc1b
H0llywood92 <manuele.volponi@gmail.com>
2021-05-12 13:43:33 +0200

    ok

--- dist/app.js
+++ dist/app.js
@@ -10,7 +10,14 @@
 var app = new Vue({
   el: '#root',
   data: {
-    navbarLink: ['Home', 'About', 'Services', 'Blog', 'Contact', 'Portfolio']
+    navbarLink: ['Home', 'About', 'Services', 'Blog', 'Contact', 'Portfolio'],
+    quickLinksFooter: ['Services', 'Contact', 'Blog'],
+    resourcesLinkFooter: ['Art Design', 'Computer', 'IT News', 'Networking', 'Web Security'],
+    contactUs: {
+      Address: 'Patricia C.Amedee 4401 Waldeck Street Grapevine Nashville, Tx 76051',
+      Phone: '+99 (0) 101 0000 888',
+      Email: 'info@yourdomain.com'
+    }
   },
   methods: {}
 });
--- src/app.js
+++ src/app.js
@@ -10,11 +10,32 @@ var app = new Vue (
                 'Blog',
                 'Contact',
                 'Portfolio'
-            ]
+            ],
+
+            quickLinksFooter: [
+                'Services',
+                'Contact',
+                'Blog'
+            ],
+
+            resourcesLinkFooter: [
+                'Art Design',
+                'Computer',
+                'IT News',
+                'Networking',
+                'Web Security'
+            ],
+
+            contactUs: {
+                Address: 'Patricia C.Amedee 4401 Waldeck Street Grapevine Nashville, Tx 76051',
+                Phone: '+99 (0) 101 0000 888',
+                Email: 'info@yourdomain.com'
+            }
         },
 
         methods: {
-
+           
         }
+        
     }
 )
